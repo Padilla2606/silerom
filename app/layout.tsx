471a89cdbea4356c4d2bd2c4b4518d0f86756d95
padilla2606 <naiyobis86@gmail.com>
2024-07-16 23:18:36 -0300

    agregando icono de pestaña

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: "/icon.png",
   title: "Silerom Digital",
   description: "Desarrollamos tu sitio web a la medida, en silerom digital te hacemos conocer en internet a travez de las redes sociales facebook, instagram",
 };

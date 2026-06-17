27d8c3785202493f6ea841a31a6fe21f2b163c1e
padilla2606 <naiyobis86@gmail.com>
2026-06-16 23:47:29 -0300

    feat: agregar SVG de community manager a tarjeta de servicios

--- app/components/services/services.tsx
+++ app/components/services/services.tsx
@@ -2,15 +2,19 @@ import Image from "next/image"
 import styles from "./services.module.css"
 
 interface ServiceCardProps {
-  icon: string
+  icons: string[]
   title: string
   description: string
 }
 
-const ServiceCard = ({ icon, title, description }: ServiceCardProps) => (
+const ServiceCard = ({ icons, title, description }: ServiceCardProps) => (
   <div className={styles.card}>
-    <div className={styles.cardIcon}>
-      <Image src={icon} width={60} height={60} alt={title} />
+    <div className={styles.cardIcons}>
+      {icons.map((icon, index) => (
+        <div key={index} className={styles.cardIcon}>
+          <Image src={icon} width={50} height={50} alt={title} />
+        </div>
+      ))}
     </div>
     <h3 className={styles.cardTitle}>{title}</h3>
     <p className={styles.cardDescription}>{description}</p>
@@ -19,19 +23,19 @@ const ServiceCard = ({ icon, title, description }: ServiceCardProps) => (
 
 const services: ServiceCardProps[] = [
   {
-    icon: "/Tarjeta/webdesing.svg",
+    icons: ["/Tarjeta/webdesing.svg"],
     title: "Desarrollo Web",
     description:
       "Diseños web a medida que capturan la esencia de tu negocio. Impulsa tu presencia en línea con nuestro servicio creativo y funcional.",
   },
   {
-    icon: "/Tarjeta/marketing.svg",
+    icons: ["/Tarjeta/marketing.svg"],
     title: "Campañas de Publicidad",
     description:
       "Llega a tu audiencia de manera efectiva con campañas publicitarias personalizadas. Maximiza tu alcance y conversión.",
   },
   {
-    icon: "/Tarjeta/diseno.svg",
+    icons: ["/Tarjeta/diseno.svg", "/Tarjeta/community.svg"],
     title: "Community Manager",
     description:
       "Gestión profesional de redes sociales para potenciar tu marca y conectar con tu audiencia. ¡Haz que tu presencia destaque!",
